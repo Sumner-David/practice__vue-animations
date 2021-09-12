746c73fbb74ae734cf85c9abca431d4b224f4bc3
David <davidphilipsumner@gmail.com>
2021-09-12 16:20:28 +0100

    Test out animations in vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,16 @@
   <div class="home">
     <Toast v-if="showToast" />
     <Todos @badValue="triggerToast" />
+
+    <button @click="showP = !showP">
+      Toggle
+    </button>
+
+    <transition name="fade">
+      <div v-if="showP">
+        Hello, Ninjas
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -20,11 +30,31 @@ export default {
       setTimeout(() => showToast.value = false, 3000)
     }
 
-    return { showToast, triggerToast }
+    const showP = ref(false)
+
+    return { showToast, triggerToast, showP }
   }
 }
 </script>
 
 <style>
+  .fade-enter-from {
+    opacity: 0
+  }
+  .fade-enter-to {
+  }
+  .fade-enter-active {
+    transition: all 0.5s linear
+  }
+
+  .fade-leave-from {
+  }
+  .fade-leave-to {
+    opacity:0
+  }
+
+  .fade-leave-active {
+    transition: all 0.5s ease
+  }
 
 </style>
