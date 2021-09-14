332cdb42f7c367777229e6e6b42ca04889854382
David <davidphilipsumner@gmail.com>
2021-09-14 20:58:10 +0100

    JavaScript animation hooks

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="about">
-    <h1>About</h1>
+
+    <transition
+      appear
+      name="fade"
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter"
+
+      @before-leave="beforeLeave"
+      @leave="leave"
+      @after-leave="afterLeave"
+    >
+      <h1 v-if="showTitle">About</h1>
+    </transition>
+
+
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
@@ -8,10 +23,52 @@
 </template>
 
 <script>
+import { ref } from '@vue/reactivity'
 
 export default {
   setup() {
 
+    const showTitle = ref(true)
+
+    const beforeEnter = (el) => {
+      console.log('before enter', el)
+
+    }
+    const enter = (el) => {
+      console.log('enter', el)
+
+    }
+    const afterEnter = (el) => {
+      console.log('after enter', el)
+
+      el.style.color="green"
+
+      setTimeout((el) => {
+        showTitle.value = false
+      }, 2000)
+
+    }
+
+
+
+    const beforeLeave = (el) => {
+      console.log('before leave', el)
+    }
+    const leave = (el) => {
+      console.log('leave', el)
+    }
+    const afterLeave = (el) => {
+      console.log('after leave', el)
+      el.style.color="pink"
+
+      setTimeout((el) => {
+        showTitle.value = false
+      }, 2000)
+
+    }
+
+
+    return { beforeEnter, enter, afterEnter, showTitle, beforeLeave, leave, afterLeave}
   }
 }
 </script>
@@ -21,4 +78,21 @@ export default {
     max-width: 600px;
     margin: 20px auto;
   }
+
+  .fade-enter-from {
+    opacity:0;
+  }
+
+  .fade-enter-active {
+    transition: opacity 3s ease;
+  }
+
+  .fade-leave-to {
+    opacity:0
+  }
+
+  .fade-leave-active {
+    transition: opacity 3s ease;
+  }
+
 </style>
