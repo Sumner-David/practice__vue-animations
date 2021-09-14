67625702326ba2e6125f4a01e97e7c2e5de29bad
David <davidphilipsumner@gmail.com>
2021-09-14 13:27:41 +0100

    Transition modes for v-if conditionals

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -6,18 +6,23 @@
       @keypress.enter="addTodo"
       placeholder="Add a new todo..."
     />
-    <div v-if="todos.length">
-      <transition-group
-        tag="ul"
-        name="list"
-        appear
-      >
-        <li v-for="todo in todos" :key="todo.id" @click="deleteTodo(todo.id)">
-          {{ todo.text }}
-        </li>
-      </transition-group>
-    </div>
-    <div v-else>Woohoo, nothing left todo!</div>
+
+    <transition name="switch" mode="out-in">
+
+      <div v-if="todos.length">
+        <transition-group
+          tag="ul"
+          name="list"
+          appear
+        >
+          <li v-for="todo in todos" :key="todo.id" @click="deleteTodo(todo.id)">
+            {{ todo.text }}
+          </li>
+        </transition-group>
+      </div>
+
+      <div v-else>Woohoo, nothing left todo!</div>
+    </transition>
   </div>
 </template>
 
@@ -122,6 +127,22 @@ export default {
   /* Move class. Lets you add items smoothly */
   .list-move {
     transition: all 0.3s ease
+  }
+
+
+  .switch-enter-from, .switch-leave-to {
+    opacity:0;
+    transform: translateY(20px)
+  }
+
+  /* This is not needed as it is the default state */
+  /* .switch-enter-to, .switch-leave-from {
+    opacity:1;
+    transform: translateY(0)
+  } */
+
+  .switch-enter-active, .switch-leave-active {
+    transition: all 0.2s ease
   }
 
 </style>
